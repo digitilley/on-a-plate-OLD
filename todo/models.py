ec2f14b9ee74b6b3bc3b1f0081a7cb0f57a56808
digitilley <jtilley451@gmail.com>
2022-09-10 14:03:15 +0000

    Linked registration page to todo_list page and made first attempt to link registration form to django

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    username = models.CharField(max_length=50, null=False, blank=False)
+    password1 = models.CharField(max_length=30, null=False, blank=False)
+    password2 = models.CharField(max_length=30, null=False, blank=False)
+
+    def __str__(self):
+        return self.username
--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
-
+from .models import User
 # Create your views here.
 
+
 def get_todo_list(request):
-    return render(request, "todo/todo_list.html")
+    users = User.objects.all()
+    context = {
+        'users': users
+    }
+    return render(request, 'todo/todo_list.html', context)
+
+
+def register_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('register_user')
+        password = 
+    return render(request, 'todo/register.html')
